a876a9b98e327ef4c31ab25f38547bc72cba2e82
Aarhat Iserv LLP <68767143+aarhatiserv@users.noreply.github.com>
2023-10-05 17:23:13 +0530

    Delete Backend directory

--- Backend/model/banner.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const bannerSchema = new mongoose.Schema({
-  imageUrl: {
-    type: String,
-  },
-});
-
-module.exports = mongoose.model("Banner", bannerSchema);
--- Backend/model/banner2.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const banner2Schema = new mongoose.Schema({
-  imageUrl: {
-    type: String,
-  },
-});
-
-module.exports = mongoose.model("Banner2", banner2Schema);
