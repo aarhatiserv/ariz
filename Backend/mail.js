19f69a6c1d0034c7c6a12997ee6453f74ed426b3
Shashankranjan-dev <shashankranjan970832@gmail.com>
2024-09-13 15:58:37 +0530

    forgot password done

--- Backend/mail.js
+++ Backend/mail.js
@@ -1,11 +1,15 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const nodemailer = require("nodemailer");
+const User = require("./model/User");
 
 router.post("/mail", (req, res) => {
   const name = req.body.name;
+
   const city = req.body.city;
   const email = req.body.email;
   const mobile = req.body.mobile;
+
   const description = req.body.description;
 
   console.log({ ...req.body });
@@ -29,23 +33,56 @@ router.post("/mail", (req, res) => {
   const mail = {
     from: name,
     to: "arizgarments91@gmail.com",
-    subject: "Contact Feedback",
+    subject: "Ariz Help Box",
     html: `<p>Name: ${name}</p>
              <p>Email: ${email}</p>
              <p>Mobile Number: ${mobile}</p>
+       >
              <p>City: ${city}</p>
-             <p>Description: ${description}</p>`,
+         
+             <p>Business Details: ${description}</p>`,
   };
 
   contactEmail.sendMail(mail, (error, info) => {
     if (error) {
-      console.error(error); // Log the error
+      console.error(error);
       res.json({ status: "ERROR" });
     } else {
       console.log("Email sent:", info.response);
       res.json({ status: "Message Sent" });
     }
   });
+});
+
+router.post("/forgot-password", async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "Email not found" });
+    }
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "arizgarments91@gmail.com",
+        pass: "hkbz ihza gyvq hxoo",
+      },
+    });
+
+    await transporter.sendMail({
+      from: "arizgarments91@gmail.com",
+      to: user.email,
+      subject: "Your Password",
+      text: `Your password is: ${user.password}`,
+    });
+
+    res.status(200).json({ message: "Password sent to your email" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 module.exports = router;
